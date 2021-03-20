72d1f5aa5d2f4ec3f99d5f16df51015dbef94b62
incompletecoder1 <80340295+incompletecoder1@users.noreply.github.com>
2021-03-20 13:52:47 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chmod +x engines/engine_name
+python lichess-bot.py
+
+# replace engine_name with the name of your engine
---
+++ startbot.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+chmod a+x engines/stockfish_13_linux_x64_modern
+ls engines -l
+python lichess-bot.py
